657e96b0d4d9813c51ca4ee2ed49353030b481f2
ShivaniAgarwal01 <justdoitshivani@gmail.com>
2025-12-28 23:46:17 +0530

    Update in Project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,14 +18,6 @@ const projects = [
       github: 'https://github.com/ShivaniAgarwal01/MindConnect',
     },
     {
-      title: 'PORTFOLIO',
-      description: 'A responsive portfolio built with React and Tailwind CSS featuring flip cards, smooth scrolling, and a clean UI to showcase projects and skills.',
-      tech: ['React','Tailwind CSS', 'HTML', 'CSS'],
-      img: '/portfolio.png',
-      github: 'https://github.com/ShivaniAgarwal01/Portfolio',
-      live: 'https://shivaniagarwal.netlify.app/',
-    },
-    {
       title: 'APPLE IPHONE 15 PRO 3D WEBSITE',
       description: 'An interactive 3D website showcasing the Apple iPhone 15 Pro, allowing users to explore the device from multiple angles. Implemented responsive design, 3D rendering, and user interaction features using modern web technologies',
       tech: ['React', 'CSS', 'GSAP' ,'Three.js' ,'JavaScript'],
